e541fb7a61aeb94573054d1959d982f64bba5097
Angel <aadrovermartinez@outlook.com>
2021-08-26 10:27:22 +0200

    Final version meteo stations

--- Station general information/station general information.py
+++ Station general information/station general information.py
@@ -1,9 +1,10 @@
 import requests as r
 import pandas as pd
+import json
 import gzip
 
 bulkdata=gzip.decompress(r.get("https://bulk.meteostat.net/v2/stations/full.json.gz").content)
 
-stations=pd.DataFrame(pd.json_normalize(bulkdata))
+stations=pd.DataFrame(json.loads(bulkdata))
 
 print(stations)
