b1d05eb02b6ce57f7d6bd5b884e077377ecd6902
Angel <47638730+SNR20db@users.noreply.github.com>
2021-12-13 15:20:35 +0100

    Update Country names.py

--- Country names/Country names.py
+++ Country names/Country names.py
@@ -9,6 +9,7 @@ df['common name'] = df['name'].apply( lambda x: x.get( 'common' ) )
 df['official name'] = df['name'].apply( lambda x: x.get( 'official' ) )
 
 my_function = lambda x: ','.join([key for key in x]) if type(x) != type( float() ) else None
+my_function_value = lambda x: ','.join([value for key, value in x]) if type(x) != type( float() ) else None
 
 df['currency'] = df['currencies'].apply( my_function )
 df['language'] = df['languages'].apply( my_function )
@@ -18,6 +19,14 @@ df['border'] = df['borders'].apply( my_function )
 df['latitude'] = df['latlng'].apply( lambda x: x[0] )
 df['longitude'] = df['latlng'].apply( lambda x: x[1] )
 
-#df['']
+df['demonym f'] = df['demonyms'].apply( lambda x: x.get( 'eng' ).get( 'f' ) if type(x) != type( float() ) else None )
+df['demonym m'] = df['demonyms'].apply( lambda x: x.get( 'eng' ).get( 'm' ) if type(x) != type( float() ) else None )
+
+df['google maps link'] = df['maps'].apply( lambda x: x.get( 'googleMaps' ) )
+
+df['gini year'] = df['gini'].apply( my_function )
+df['gini value'] =  df['gini'].apply( lambda x: ','.join( [ str( value ) for value in list( x.values() ) ] ) if type(x) != type( float() ) else None )
+
+df['car side'] = df['car'].apply( lambda x: x.get( 'side' )  )
 
 print( df )
