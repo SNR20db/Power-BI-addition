7980ca8e57a6d353a4921baffb9a5642088798c6
Angel <47638730+SNR20db@users.noreply.github.com>
2021-12-13 13:50:55 +0100

    Update Country names.py

--- Country names/Country names.py
+++ Country names/Country names.py
@@ -8,6 +8,13 @@ df = pd.DataFrame( response.json() )
 df['common name'] = df['name'].apply( lambda x: x.get( 'common' ) )
 df['official name'] = df['name'].apply( lambda x: x.get( 'official' ) )
 
-#df['currency'] = df['currencies'].apply( lambda x:  x )
+my_function = lambda x: ','.join([key for key in x]) if type(x) != type( float() ) else None
+
+df['currency'] = df['currencies'].apply( my_function )
+df['language'] = df['languages'].apply( my_function )
+df['capital city'] = df['capital'].apply( my_function )
+
+df['latitude'] = df['latlng'].apply( lambda x: x[0] )
+df['longitude'] = df['latlng'].apply( lambda x: x[1] )
 
 print( df )
